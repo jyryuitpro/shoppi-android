709219a50197d668f6a73986db2a3c89bff1a7d1
jyr <jyr@wavers.co.kr>
2022-05-15 16:34:55 +0900

    Jetpack Navigation

--- app/src/main/java/io/jyryuitpro/shoppi/app/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/app/MainActivity.kt
@@ -3,6 +3,8 @@ package io.jyryuitpro.shoppi.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 private const val TAG = "MainActivity"
@@ -16,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
         bottomNavigationView.itemIconTintList = null
 
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
         Log.d(TAG, "onCreate")
     }
 
