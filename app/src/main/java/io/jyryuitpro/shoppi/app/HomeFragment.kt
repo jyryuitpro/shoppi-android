fcf4e8d6e05b542df5e04a87194da63ad5f1b781
jyr <jyr@wavers.co.kr>
2022-05-15 18:59:18 +0900

    실습 : JSON 데이터 파싱

--- app/src/main/java/io/jyryuitpro/shoppi/app/HomeFragment.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/app/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -31,5 +32,23 @@ class HomeFragment : Fragment() {
         val assetLoader = AssetLoader()
         val homeData = assetLoader.getJsonString(requireContext(), "home.json")
         Log.d("homeData", homeData ?: "")
+
+        if (!homeData.isNullOrEmpty()) {
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+            val titleValue = Title(text, iconUrl)
+            titleValue.text
+
+//            val topBanners = jsonObject.getJSONArray("top_banners")
+//            val firstBanner = topBanners.getJSONObject(0)
+//            val label = firstBanner.getString("label")
+//            val productDetail = firstBanner.getJSONObject("product_detail")
+//            val price = productDetail.getInt("price")
+
+//            Log.d("title", "text=${text}, iconUrl=${iconUrl}")
+//            Log.d("firstBanner", "label=${label}, price=${price}")
+        }
     }
 }
---
+++ app/src/main/java/io/jyryuitpro/shoppi/app/Title.kt
@@ -0,0 +1,6 @@
+package io.jyryuitpro.shoppi.app
+
+data class Title(
+    val text: String,
+    val iconUrl: String
+)
