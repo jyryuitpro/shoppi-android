43969ffcb42dbecb45c4a1b7fabc6a9c0b3c3edd
jyr <jyr@wavers.co.kr>
2022-05-15 15:34:17 +0900

    실습 1. Bottom Navigation

--- app/src/main/java/io/jyryuitpro/shoppi/app/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/app/MainActivity.kt
@@ -3,6 +3,7 @@ package io.jyryuitpro.shoppi.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 private const val TAG = "MainActivity"
 
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
+
         Log.d(TAG, "onCreate")
     }
 
