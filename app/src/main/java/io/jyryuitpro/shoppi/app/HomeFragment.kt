a32c9230b35aaa1b34787e03b998da81efd4aa0e
jyr <jyr@wavers.co.kr>
2022-05-15 19:40:46 +0900

    과제 해설 1 - 요구사항 구현

--- app/src/main/java/io/jyryuitpro/shoppi/app/HomeFragment.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/app/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.json.JSONObject
@@ -21,13 +23,15 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-        button.setOnClickListener {
+//        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+//        button.setOnClickListener {
 //            val transaction = parentFragmentManager.beginTransaction()
 //            transaction.add(R.id.container_main, ProductDetailFragment())
 //            transaction.commit()
-            findNavController().navigate(R.id.action_home_to_product_detail)
-        }
+//            findNavController().navigate(R.id.action_home_to_product_detail)
+//        }
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
 
         val assetLoader = AssetLoader()
         val homeData = assetLoader.getJsonString(requireContext(), "home.json")
@@ -38,9 +42,11 @@ class HomeFragment : Fragment() {
             val title = jsonObject.getJSONObject("title")
             val text = title.getString("text")
             val iconUrl = title.getString("icon_url")
-            val titleValue = Title(text, iconUrl)
-            titleValue.text
-
+//            val titleValue = Title(text, iconUrl)
+            toolbarTitle.text = text
+            GlideApp.with(this)
+                .load(iconUrl)
+                .into(toolbarIcon)
 //            val topBanners = jsonObject.getJSONArray("top_banners")
 //            val firstBanner = topBanners.getJSONObject(0)
 //            val label = firstBanner.getString("label")
