530f5f01bd1c1cca9a2482ad883030fe0ed389ff
jyr <jyr@wavers.co.kr>
2022-08-15 10:56:52 +0900

    실습 : ViewPager - 3. JSON 데이터 파싱 with gson && 실습 : ViewPager - 4. 데이터 할당

--- app/src/main/java/io/jyryuitpro/shoppi/android/HomeBannerAdapter.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/android/HomeBannerAdapter.kt
@@ -1,5 +1,7 @@
 package io.jyryuitpro.shoppi.android
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import java.text.DecimalFormat
+import kotlin.math.roundToInt
 
 class HomeBannerAdapter : ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHolder>(BannerDiffCallback()) {
 
@@ -33,9 +37,33 @@ class HomeBannerAdapter : ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHo
         private val bannerDetailProductPriceTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_price)
 
         fun bind(banner: Banner) {
+            loadImage(banner.backgroundImageUrl, bannerImageView)
+
+            bannerBadgeTextView.text = banner.badge.label
+            bannerBadgeTextView.background = ColorDrawable(Color.parseColor(banner.badge.backgroundColor))
+            bannerTitleTextView.text = banner.label
+            loadImage(banner.productDetail.thumbnailImageUrl, bannerDetailThumbnailImageView)
+            bannerDetailBrandLabelTextView.text = banner.productDetail.brandName
+            bannerDetailProductLabelTextView.text = banner.productDetail.label
+            bannerDetailProductDiscountRateTextView.text = "${banner.productDetail.discountRate}%"
+            calculateDiscountAmount(bannerDetailProductDiscountPriceTextView, banner.productDetail.discountRate, banner.productDetail.price)
+            applyPriceFormat(bannerDetailProductPriceTextView, banner.productDetail.price)
+        }
+
+        private fun calculateDiscountAmount(view: TextView, discountRate: Int, price: Int) {
+            val discountPrice = (((100 - discountRate) / 100.0) * price).roundToInt()
+            applyPriceFormat(view, discountPrice)
+        }
+
+        private fun applyPriceFormat(view: TextView, price: Int) {
+            val decimalFormat = DecimalFormat("#,###")
+            view.text = decimalFormat.format(price) + "원"
+        }
+
+        private fun loadImage(urlString: String, imageView: ImageView) {
             GlideApp.with(itemView)
-                .load(banner.backgroundImageUrl)
-                .into(bannerImageView)
+                .load(urlString)
+                .into(imageView)
         }
     }
 }
--- app/src/main/java/io/jyryuitpro/shoppi/android/HomeFragment.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/android/HomeFragment.kt
@@ -10,6 +10,9 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.gson.Gson
 import org.json.JSONObject
 
 class HomeFragment : Fragment() {
@@ -36,37 +39,51 @@ class HomeFragment : Fragment() {
 
         val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
         val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
 
         val assetLoader = AssetLoader()
 //        assetLoader.getJsonString(context, "home.json")
-        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
-        Log.d("homeData", homeData ?: "")
+//        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+//        Log.d("homeData", homeData ?: "")
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
 
-        if (!homeData.isNullOrEmpty()) {
-            val jsonObject = JSONObject(homeData)
-            val title = jsonObject.getJSONObject("title")
-            val text = title.getString("text")
-            val iconUrl = title.getString("icon_url")
+        if (!homeJsonString.isNullOrEmpty()) {
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
+
+//            val jsonObject = JSONObject(homeJsonString)
+//            val title = jsonObject.getJSONObject("title")
+//            val text = title.getString("text")
+//            val iconUrl = title.getString("icon_url")
 //            val titleValue = Title(text, iconUrl)
 //            toolbarTitle.text = titleValue.text
-            toolbarTitle.text = text
+//            toolbarTitle.text = text
+            toolbarTitle.text = homeData.title.text
 
 //            GlideApp.with(this)
 //                .load(titleValue.iconUrl)
 //                .into(toolbarIcon)
 
+//            GlideApp.with(this)
+//                .load(iconUrl)
+//                .into(toolbarIcon)
+
             GlideApp.with(this)
-                .load(iconUrl)
+                .load(homeData.title.iconUrl)
                 .into(toolbarIcon)
 
-            val topBanners = jsonObject.getJSONArray("top_banners")
-            val firstBanner = topBanners.getJSONObject(0)
-            val label = firstBanner.getString("label")
-            val productDetail = firstBanner.getJSONObject("product_detail")
-            val price = productDetail.getInt("price")
+//            val firstBanner = topBanners.getJSONObject(0)
+//            val label = firstBanner.getString("label")
+//            val productDetail = firstBanner.getJSONObject("product_detail")
+//            val price = productDetail.getInt("price")
 
-            Log.d("text", "text=${text}, iconUrl=${iconUrl}")
-            Log.d("firstBanner", "label=${label}, price=${price}")
+//            Log.d("text", "text=${text}, iconUrl=${iconUrl}")
+//            Log.d("firstBanner", "label=${label}, price=${price}")
+
+            viewpager.adapter = HomeBannerAdapter().apply {
+                submitList(homeData.topBanners)
+            }
         }
     }
 }
