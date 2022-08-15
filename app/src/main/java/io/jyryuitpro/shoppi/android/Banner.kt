41e0966238eeaad07b8eb6b3181d3798db3a82ea
jyr <jyr@wavers.co.kr>
2022-08-15 10:07:41 +0900

    실습 : ViewPager - 2. Adapter 구현

---
+++ app/src/main/java/io/jyryuitpro/shoppi/android/Banner.kt
@@ -0,0 +1,22 @@
+package io.jyryuitpro.shoppi.android
+
+data class Banner(
+    val backgroundImageUrl: String,
+    val badge: BannerBadge,
+    val label: String,
+    val productDetail: ProductDetail
+)
+
+data class BannerBadge(
+    val label: String,
+    val backgroundColor: String
+)
+
+data class ProductDetail(
+    val brandName: String,
+    val label: String,
+    val discountRate: Int,
+    val price: Int,
+    val thumbnailImageUrl: String,
+    val productId: String
+)
