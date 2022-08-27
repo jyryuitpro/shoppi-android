b3e332181434f8c7e2564736106dafb1ef909963
jyr <jyr@wavers.co.kr>
2022-08-27 22:38:29 +0900

    실습 : 화면 전환 구현

--- app/src/main/java/io/jyryuitpro/shoppi/android/ui/category/CategoryAdapter.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/android/ui/category/CategoryAdapter.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import io.jyryuitpro.shoppi.android.databinding.ItemCategoryBinding
 import io.jyryuitpro.shoppi.android.model.Category
 
-class CategoryAdapter : ListAdapter<Category, CategoryAdapter.CategoryViewHolder>(CategoryDiffCallback()) {
+class CategoryAdapter(private val viewModel: CategoryViewModel) : ListAdapter<Category, CategoryAdapter.CategoryViewHolder>(CategoryDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
         val binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -19,9 +19,10 @@ class CategoryAdapter : ListAdapter<Category, CategoryAdapter.CategoryViewHolder
         holder.bind(getItem(position))
     }
 
-    class CategoryViewHolder(private val binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class CategoryViewHolder(private val binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(category: Category) {
+            binding.viewModel = viewModel
             binding.category = category
             binding.executePendingBindings()
         }
