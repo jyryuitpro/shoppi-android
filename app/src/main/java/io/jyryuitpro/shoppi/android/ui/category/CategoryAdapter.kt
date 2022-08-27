e06c7ed537ab2b23e05ba57fe9f0ec733c21a0c4
jyr <jyr@wavers.co.kr>
2022-08-27 19:43:16 +0900

    실습 : RecyclerView - 2. Adapter 구현 with data binding

---
+++ app/src/main/java/io/jyryuitpro/shoppi/android/ui/category/CategoryAdapter.kt
@@ -0,0 +1,39 @@
+package io.jyryuitpro.shoppi.android.ui.category
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import io.jyryuitpro.shoppi.android.databinding.ItemCategoryBinding
+import io.jyryuitpro.shoppi.android.model.Category
+
+class CategoryAdapter : ListAdapter<Category, CategoryAdapter.CategoryViewHolder>(CategoryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        val binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class CategoryViewHolder(private val binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(category: Category) {
+            binding.category = category
+            binding.executePendingBindings()
+        }
+    }
+}
+
+class CategoryDiffCallback : DiffUtil.ItemCallback<Category>() {
+    override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem.categoryId == newItem.categoryId
+    }
+
+    override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem == newItem
+    }
+}
