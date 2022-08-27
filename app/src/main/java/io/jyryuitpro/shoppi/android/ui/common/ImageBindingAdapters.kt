85b586952c947d7120e6f9a191e884eda51bb5e6
jyr <jyr@wavers.co.kr>
2022-08-28 01:59:32 +0900

    실습 : 중첩 RecyclerView - 1. layout 구현

--- app/src/main/java/io/jyryuitpro/shoppi/android/ui/common/ImageBindingAdapters.kt
+++ app/src/main/java/io/jyryuitpro/shoppi/android/ui/common/ImageBindingAdapters.kt
@@ -12,3 +12,13 @@ fun loadImage(view: ImageView, imageUrl: String?) {
             .into(view)
     }
 }
+
+@BindingAdapter("circleImageUrl")
+fun loadCircleImage(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        GlideApp.with(view)
+            .load(imageUrl)
+            .circleCrop()
+            .into(view)
+    }
+}
